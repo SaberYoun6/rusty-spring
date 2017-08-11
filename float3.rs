23285870e814905808cbc4abe4dc562a2fa9c987
yuriprym <samuel.young.103@gmail.com>
2017-08-10 19:42:02 -0600

    removed not nessacry code because it was weighting me down and am now currently correcting code

--- float3.rs
+++ float3.rs
@@ -1,11 +1,10 @@
- use branchPrediction::*;
- use streflop_cond::*;
- use rustreg_cond::*;
- use fastmath::*;
- use win64::*;
- use libc::*;
+ //use branchPrediction::*;
+// use streflop_cond::*;
+// use rustreg_cond::*;
+// use fastmath::*;
+ //use win64::*
+ extern crate core;
  use std::env::*;
- use std::f64::consts;
  use core::f64::consts;
  use std::default::*;
  use std::ops::DivAssign;
@@ -25,55 +24,57 @@ impl float3{
     fn origin() -> float3 {
         float3 { x : 0.0, y: 0.0, z : 0.0 }
     }
-    fn new(x : f64 ,y :f64 , z:f64) -> float3 {
-        float3 {x : x, y: y, z: z }
+    fn new(x : f64 ,y : f64 , z : f64) -> float3 {
+        float3 {x : x, y : y, z : z }
     }
 }
-        
-struct float {
+ /*       
+struct floater {
     f : f64,
 }
-impl float {
-    fn origin() -> float{
-        float { f : 0.0 }
+impl floater {
+    fn origin() -> f64{
+        floater { f : 0.0 }
     }
-    fn new ( f : f64) {
-        float{f :f }
+    fn new ( f : f64)-> f64 {
+        floater {f : f }
     }
 }
 struct float1{
-    f:[f64;3],
+    fo:[f64;3],
 }
 impl float1 {
   fn origin() -> float1 {
-        float1 { f : 0.0  , f : 0.0  ,f : 0.0}
+        float1 ();
   }
-  fn new (f:[f64;3]) ->float1 {
-      float1 { f : f , f : f , f :f }
+  fn new (fo:[f64;3]) ->float1 {
+      float1 ( fo : flo);
   }     
 }
 struct float2{
-    f: float3,
+    fl : float3,
 }
 impl float2 {
     fn origin() -> float2 {
-        float2 { f : 0.0 }
+        float2 ();
     }
-    fn new ( f : float3) {
-        float2 { f : f }
+    fn new ( fl : float3) {
+        float2 ( fl : flo );
     } 
 }
+
 struct vector {
     vectors : float3,
 }
 impl vector {
    fn  origin_vector() -> float3 {
-       vector { vectors:0.0, vectors: 0.0,vectors : 0.0}
+       vector { vectors :0.0}
    }
    fn new_vector(vectors : float3) {
-       vector{ vectors : vectors, vectors : vectors, vectors : vectors }
+       vector{ vectors : vectors}
    }
 }
+*/
 struct int_float3{
     t: i64 ,
 }
@@ -86,7 +87,7 @@ impl int_float3 {
     }
 }
 struct float_maxxpos{
-    f64 : maxxpos,
+   maxxpos : f64,
 }
 impl float_maxxpos {
     fn origin_maxxpos() -> float_maxxpos {
@@ -97,7 +98,7 @@ impl float_maxxpos {
     }
 }
 struct float_maxypos {
-    f64 : maxypos,
+    maxypos : f64,
 }
 impl float_maxypos {
     fn origin_maxypos() -> float_maxypos {
@@ -107,289 +108,30 @@ impl float_maxypos {
         float_maxypos{ maxypos : maxypos}
     }
 }
-impl floati3 for float_maxxpos + float3 (x,y) + float_maxypos {
-    fn is_in_bounds() -> bool{
-        assert!(maxxpos > 0.0);
-        return (x>= 0.0 && x <= maxxpos) && (y >= 0.0 &&  y <= maxypos);
-    }
-    fn is_in_map() -> bool{
-        assert!(maxxpos > 0.0);
-        return (x >= 0.0 && x <= maxxpos+1) && (y >= 0.0 && y <= maxypos +1);
+struct floati3
+{
+
+}
+
+impl floati3 {
+    fn is_in_bounds( maxx : f64 , maxy : f64 ,  x : f64 , y :f64) -> bool 
+    {
+        assert!(maxx > 0.0);
+        return(x >= 0.0 && x <= maxx  ) && ( y >= 0.0 && y <= maxy);
     }
 }
-    /*
-    fn clamp_in_map() {
-        assert!(float3_maxxpos > 0.0);
-        let f64: x = clamp(x, 0.0, maxxpos+1);
-        let f64: y = clamp(y, 0.0, maxypos+1);
-    }
-    fn min(float3: v1,float3: v2)
-    {
-        return float3(v1.x.min(v2.x), v1.y.min(v2.y), v1.z.min(v2.z));
-    }
-    fn cmp_eps() ->f64 {
-        return 1e-04;
-    }
-    fn nrm_eps() ->f64{
-        return 1e-12;
-    }
-    fn max(float3: v1, float3: v2){
-        return float3(v1.x.max(v2.x), v1.y.max(v2.y), v1.z.max(v2.z));
-    }
-    fn fabs( float3: v){
-        return float3(v.x.abs(), v.y.abs(), v.z.abs());
-    }
-    fn float3(f64: x, f64: y ,f64: z )  {
-        let mut f64: x=x(x);
-        let mut f64: y=y(y);
-        let mut f64: z=z(z);
-    }
-    fn float3_a( f: [f64;3])  {
-        let x=x(f[0]);
-     let y=y(f[1]);
-        let z=z(f[2]);
 
-    }
-    fn operator_equ(&mut self ,f:[f64;3])-> float3 {
-       let float3: x =f[0];
-       let float3: y =f[1];
-       let float3: z =f[2];
-        return *self;
-    }
-    fn operator_add(float3: f, &self)-> float3 {
-        return  float3(x+self.x, y+self.y, z+self.z);
-    }
-    fn copy_into(f:[f64;3]) {
-        let mut f:[f64;0] =x;
-        let mut f:[f64;1] =y;
-        let mut f:[f64;2] =z;
-    }
-    fn operator_add_equ(&mut self , other : pointer )-> float3 {
-       let x = add_assign(self.x,other.x);
-       let y = add_assign(self.y,other.y);
-       let z = add_assign(self.z,other.z);
-       return self;
-    }
-    fn operator_min (float3: f, Self : f) ->float3 {
-    return float3(self.x-f, self.y-f, self.z-f);
-    }
-    fn operator_negative(f64: x,f64:  y,f64: z)->float3  {
-        return float3(-x,-y,-z);
-    }
-    fn operator_min_equ(float3: f,&mut self) {
-        let float3:  x = sub_assgin(self.x,f);
-        let float3:  y = sub_assign(self.y.f);
-        let float3:  z =sub_assign(self.z,f);
-    }
-    fn operator_muilt (float3: f, &mut self )->float3 {
-        return float3(self.x*f, self.y*f, self.z*f);
-    }
-    fn operator_muilt_equal(float3: f, &mut self) {
-        let float3 :x = mul_assign(f,self.x);
-        let float3 :y = mul_assign(f,self.y);
-        let float3 :z = mul_assign(f,self.z);
-    }
-    fn operator_muilt_equals (f64: x, &mut self ,)->*mut self {
-        let  x = mul_assign(f,self.x);
-        let  y = mul_assign(f,self.y);
-        let  z = mul_assign(f.self.z);
-        return *self;
-    }
-    fn operator_div (float3: f, &mut self)->float3 {
-        return float3(self.x/f,self.y/f,self.z/f);
-    }
-    fn operator_inver  (f64: f, &mut self)-> f64 {
-        const f64: inv = 1.0 /self;
-        return (*self) * inv;
-    }
-    fn operator_divide_equa(float3: f,&mut self) {
-        let float3: x = div_assign(f,self.x);
-        let float3: y = div_assign(f,self.y); 
-        let float3: z = div_assign(f,self.z);
-    }
-    fn operator_division_equal(f64 : f, &mut self) {
-        const f64 :inv = 1.0 /self;
-        *self *= inv;
-    }
-    fn operator_equ_equ(float3: f) -> bool {
-        return equal(f);
-    }
-    fn operator_not_equ(float3: f)-> bool{ 
-        return !(equals(f));
-    }
-    fn operator_array (i64 : t) -> inflow {
-        type inflow = &x<a:[t;i64]>;
-        return inflow; 
-    }
-    fn operator_arrays(i64 : t) -> outflow {
-         type outflow = &x<a:[t;i64]>;
-         return outflow;
-    }
-    fn equals(float3:  f ,  float3 : eps,&mut  self)->bool  {
-       return epscmp(self.x.f,eps) && epscmp(self.y,f,eps) && epscmp(self.z,f,eps);
-    }
-    fn same(float3: f, &mut self) -> bool {
-        return self.x == f && self.y == f && self.z ==f;
-    }
-    fn dot(float3 : f,&mut self) ->f64  {
-        return (self.x * f) + (self.y * f) + (self.z *f);
-    }
-    fn dot_2d(float3: f,&mut  self ) {
-        return (self.x * f) + (self.y * f.y);
-    }
-    fn cross(float3: f,&mut  self)->float3 {
-        return float3[ f64
-            (self.y*f) - (self.z*f)
-            (self.z*f) - (self.x*f)
-            (self.x*f) - (self.y*f)];
-    }
-    fn distance(float3:  f,&mut  self)->f64 {
-        let f64: dx=self.x - f;
-        let f64: dy=self.y - f;
-        let f64: dz=self.z - f;
-        let mut f64: dist=(dx*dx)+(dy*dy)+(dz*dz);
-        return dist.sqrt();
-    }
-    fn distance2d(float3 : f, &mut self) ->f64 {
-        let f64: dx = self.x-f;
-        let f64: dy = self.y-f;
-        let mut f64: dist2 = (dx*dx+dy*dy);
-        return dist2.sqrt();
-    }
-    fn length() {
-        //assert!(x !=0.0f || y !=0.0f ||z !=0.0f);
-        return sq_length().sqrt();
-    }
-    fn length_2d() {
-        return sq_length_2d().sqrt();
-    }
-    fn length_normalize(&mut self )-> f64 {
-        const f64: len = length();
-        if likely(len > nrm_eps())
-        {
-            *self = mul_assign((1.0 / len), *self);
-        }
-        return len;
-    }
-    fn length_nomralize_2d (&mut self) -> f64{
-        let f64: len = length_2d();
-        if likely(len > nrm_eps())
-        {
-            y =0.0; 
-             *self= mul_assign((1.0/len),*self);
-        }
-        return len;
-    }
-    fn normalize() {
-        if defined(__SUPPORT_SNAN__){
-            if !(BUILDING_AI){
-                return safe_normalize();
-            }
-            assert!(sq_length()> nrm_eps());
-            return unsafe_normalize();
-        } else {
-            return safe_normalize();
-	}
-    }
-    fn normalize_2d() {
-        let f64 : y =  0.0;
-        return normalize();
-    }
-    fn unsafe_normalize(&mut self)->*mut self {
-         *self = mult_assign(sq_length().sqrt(), *self);
-        return *self;
-    }
-    fn unsafe_normalize_2d() {
-        let f64 : y = 0.0;
-        return unsafe_normalize();
-    }
-    fn safe_normalize(&mut self)-> *mut self {
-        let f64: sql = sq_length();
-        if  likely(sql>nrm_eps())
-        {
-            *self = mul_assign(sql.sqrt(),*self);
-        }
-        return *self;
-    }
-    fn safe_normalize_2d() {
-        let f64 : y = 0.0;
-        return safe_normalize();
-    }
-    fn a_normalize() {
-        if defined(__SUPPORT_SNAN__){
-        if !(BUILDING_AI){
-            return safe_a_normalize();
-        }
-        assert!(sq_length() > nrm_eps());
-        return unsafe_a_normalize();
-        }  else {
-            return safe_a_normalize();
-	}
-    }
-    fn a_normalize_2d() {
-        let f64 : y= 0.0 ;
-        return a_nomralize();
-    }
-    fn safe_a_normalize(&mut self)->*mut self {
-        const f64: sql = sq_length();
-        if likely(sql > nrm_eps()){
-           *self =  mul_assign(sql.sqrt(),(*self));
-        }
-        return *self;
-    }
-    fn sq_length() ->f64  {
-        return x*x +y*y+ z*z;
-    }
-    fn sq_length_2d()->f64 {
-        return x*x + y*y;
-    }
-    fn sq_distance(float3: f,&mut self)->f64  {
-        let f64: dx = self.x-f;
-        let f64: dy = self.y-f;
-        let f64: dz = self.z-f;
-        return dx*dx +dy*dy + dz*dz;
-    }
-    fn sq_distance_2d(mut float3: f, Self : f)-> f64  {
-        let f64: dx =self.x-f;
-        let f64: dy = self.y-f;
-        return dx*dx + dy*dy;
-    }
-    fn assert_nans() {
-        assert!(!x.is_nan() && !x.is_infinite());
-        assert!(!y.is_nan() && !y.is_infinite());
-        assert!(!z.is_nan() && !z.is_infinite());
-    }
-    */
 
 fn main(){
-    let maxxpos1= float_maxxpos{-1.2};
-    let min =float3::float3(1.345,4.5231,5.67902);
-    let max =float3::float3(8.888,35.8913,10.9123);
-    let x = 2.890;
-    let y = 3.345;
-    let z = 9.1235;
-    let asert = float3::float3(x,y,z);
-    if maxxpos1.is_in_bounds() {
+    let &float_maxxpos : maxxpos1= float_maxxpos::new_maxxpos(-1.2);
+    let &float_maxypos :  maxypos1= float_maxypos::new_maxypos(1.0);
+    let x = 1.0987;
+    let y = 2.9080;
+    let z = 8.9123;
+    let wxz=float3::new(x,y,z);
+    if floati3::is_in_bounds(maxxpos1, maxypos1, x, y) {
         println!("then this function is working");
     } else {
        println!("there is no results");
     }
-    if !(maxxpos1.is_in_map()) {
-	println!("there was no results");
-    } else {
-    println!("there is a result");
-    }
-     //   println!("{} is set to the float3", float3::sq_distance(asert,asert));
-     //   println!("{} is set to the float3", float3::sq_distance_2d(max,max));
 }
-
-    /*
-    println!(float3.sq_distance_2d(max));
-
-    println!(float3.distance(asert));
-    println!(float3.distance_2d(max));
-    pritnln!(float3.dot(min));
-    println!(float3.min(min, max));
-    println!(float3.max(asert,min));
-    */
-//}
